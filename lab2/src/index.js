34af736672bf9d5fced5b79095789842934393cd
DogNellaf <dans-host@ya.ru>
2023-08-29 17:27:41 +0300

    Lab2

--- lab2/src/index.js
+++ lab2/src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import './bootstrap.min.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
 
-reportWebVitals();
+root.render(<App />);
